692c95a9f24a5b94d37d5e421c208289109c572c
DeadSwim <66752697+DeadSwimek@users.noreply.github.com>
2023-12-06 11:53:34 +0100

    Add files via upload

---
+++ CSSReservation.cs
@@ -0,0 +1,90 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Timers;
+using System.ComponentModel;
+
+namespace CSSReservation;
+[MinimumApiVersion(100)]
+
+public partial class CSSReservation : BasePlugin, IPluginConfig<ConfigRes>
+{
+    public override string ModuleName => "Reservation slot";
+    public override string ModuleAuthor => "DeadSwim";
+    public override string ModuleDescription => "Simple reservation kicking.";
+    public override string ModuleVersion => "V. 1.0.0";
+
+
+
+    public ConfigRes Config { get; set; }
+    public int PlayerConnected;
+
+    public void OnConfigParsed(ConfigRes config)
+    {
+        Config = config;
+    }
+
+    public override void Load(bool hotReload)
+    {
+        RegisterListener<Listeners.OnMapStart>(name =>
+        {
+            PlayerConnected = 0;
+
+        });
+        RegisterListener<Listeners.OnClientAuthorized>((index, id) =>
+        {
+            var player = Utilities.GetPlayerFromSlot(index);
+
+            Authorization_Client(player);
+        });
+    }
+    public void Authorization_Client(CCSPlayerController player)
+    {
+        var client = player.Index;
+        var maxplayers = Server.MaxPlayers;
+
+        int connected = 0;
+        foreach (var player_l in Utilities.GetPlayers().Where(player => player is { IsBot: false, IsValid: true }))
+        {
+            connected++;
+        }
+        PlayerConnected = connected;
+        Server.PrintToConsole($"CSS RESERVATION - Player {player.PlayerName} try to connect on server. Actuall players on server {PlayerConnected}");
+        bool kicked = false;
+        if (PlayerConnected == Server.MaxPlayers)
+        {
+            if (AdminManager.PlayerHasPermissions(player, $"{Config.permission}"))
+            {
+                foreach (var l_player in Utilities.GetPlayers())
+                {
+                    CCSPlayerController player_res = l_player;
+
+                    var el_player = player_res.Index;
+                    if (kicked == false)
+                    {
+                        if (AdminManager.PlayerHasPermissions(player, $"{Config.permission}"))
+                        {
+                            kicked = true;
+                            Server.PrintToChatAll($" {Config.Prefix}Player {ChatColors.Lime}{player_res.PlayerName} {ChatColors.Default}has been kicked, bcs {ChatColors.Lime}Admin{ChatColors.Default} need to connect.");
+                            Server.ExecuteCommand($"kickid {player_res.UserId}");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Server.ExecuteCommand($"kickid {player.UserId}");
+            }
+        }
+        else
+        {
+            Server.ExecuteCommand($"kickid {player.UserId}");
+        }
+        kicked = false;
+    }
+}
---
+++ Configs.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Utils;
+using System.Text.Json.Serialization;
+
+namespace CSSReservation;
+
+
+public class ConfigRes : BasePluginConfig
+{
+    [JsonPropertyName("Prefix")] public string Prefix { get; set; } = $" {ChatColors.Default}[{ChatColors.Green}MadGames.eu{ChatColors.Default}]";
+    [JsonPropertyName("permission")] public string permission { get; set; } = "@css/reservation";
+
+
+}
